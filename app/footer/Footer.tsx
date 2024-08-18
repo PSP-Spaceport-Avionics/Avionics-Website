35ee9b5a6da7bd530b0bf745efe983aa825f0054
123kevinma123 <123kevinma123@gmail.com>
2024-08-18 19:43:47 -0400

    side scroller

--- app/footer/Footer.tsx
+++ app/footer/Footer.tsx
@@ -4,7 +4,9 @@ import './Footer.css';
 
 function Footer() {
 	return (
-		<footer className='footer items-center justify-center'>Footer</footer>
+		<footer className='footer flex items-center justify-center font-extralight tracking-wide'>
+			Avionics &copy; 2024
+		</footer>
 	);
 }
 
